4c98c1609fe9f97433aa7b40b99070e74fcb556b
mimosasaurus <greglrankin@gmail.com>
2023-10-01 19:23:32 -0300

    cleaned up questions

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 import inquirer from 'inquirer';
 // TODO: Create an array of questions for user input
 const questions = [
-    {type: "input", name: "A", default: "B", validate: false, message: "What is your GitHub username?"},
-    {type: "input", name: "A", default: "B", validate: false, message: "What is your email address?"},
-    {type: "input", name: "A", default: "B", validate: false, message: "What is your project's title?"},
-    {type: "input", name: "A", default: "B", validate: false, message: "Please write a short description of your project"},
-    {type: "input", name: "A", default: "B", validate: false, message: "What license should be on your project?"},
-    {type: "input", name: "A", default: "B", validate: false, message: "What command should be run to install dependencies?"},
-    {type: "input", name: "A", default: "B", validate: false, message: "What command should run to run tests?"},
-    {type: "input", name: "A", default: "B", validate: false, message: "What does the user need to know about using the repository?"},
-    {type: "input", name: "A", default: "B", validate: false, message: "What does the user need to know about contrubuting to the repository?"}
+    {type: "input", name: "A", message: "What is your GitHub username?"},
+    {type: "input", name: "A", message: "What is your email address?"},
+    {type: "input", name: "A", message: "What is your project's title?"},
+    {type: "input", name: "A", message: "Please write a short description of your project"},
+    {type: "input", name: "A", message: "What license should be on your project?"},
+    {type: "input", name: "A", message: "What command should be run to install dependencies?"},
+    {type: "input", name: "A", message: "What command should run to run tests?"},
+    {type: "input", name: "A", message: "What does the user need to know about using the repository?"},
+    {type: "input", name: "A", message: "What does the user need to know about contrubuting to the repository?"}
 ]
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
