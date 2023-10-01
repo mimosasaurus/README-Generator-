126361fd5098d639e413cc4a67d0faf502b063b8
mimosasaurus <greglrankin@gmail.com>
2023-10-01 18:57:13 -0300

    added questions

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What is your project's title?",
+    "Please write a short description of your project",
+    "What license should be on your project?",
+    "What command should be run to install dependencies?",
+    "What command should run to run tests?",
+    "What does the user need to know about using the repository?",
+    "What does the user need to know about contrubuting to the repository?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
